553242c18e5c1f4755d87110d16b08e53be232a2
RMiklos2 <rmiklos2@huskers.unl.edu>
2024-08-28 10:56:13 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Riley Miklos
+ * Date: 08/28/2024
+ *
+ * A simple program to print my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Riley Miklos\n");
+
+    return 0;
+}
